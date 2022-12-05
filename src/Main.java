d23197c8264e7ee062f5cfc041cf59bc185589aa
EriikaRuohonen <113431578+EriikaRuohonen@users.noreply.github.com>
2022-12-05 20:56:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Muodostetaan olio
+		Pyykkikone olio1 = new Pyykkikone();
+		olio1.merkki = "Upo";
+		olio1.malli = "Pesukarhu";
+		olio1.tayttomaara = 7;
+	}
+
+}
+
+class Pyykkikone {
+		// Ominaisuudet
+		String merkki;
+		String malli;
+		int tayttomaara;
+		
+		}
+
