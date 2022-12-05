d43102f0f044e80585553b4cabb2ca5cad903b15
EriikaRuohonen <113431578+EriikaRuohonen@users.noreply.github.com>
2022-12-05 22:25:38 +0200

    Toka commit

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Muodostetaan olio
-		Pyykkikone olio1 = new Pyykkikone();
-		olio1.merkki = "Upo";
-		olio1.malli = "Pesukarhu";
-		olio1.tayttomaara = 7;
-		olio1.Kaynnisty();
+		Pyykkikone kone1 = new Pyykkikone("Upo", "Pesukarhu", 7);
+		Pyykkikone kone2 = new Pyykkikone("Siemens", "PK1000S", 8);
+		kone1.tulostaTiedot();
+		kone1.Kaynnisty();
 	}
 
 }
 
 class Pyykkikone {
+	
 		// Ominaisuudet
-		String merkki;
-		String malli;
-		int tayttomaara;
+		public String merkki;
+		public String malli;
+		public int tayttomaara;
+		
+		// Luodaan konstruktori
+		public Pyykkikone(String merkki, String malli, int tayttomaara)
+		{
+			this.merkki = merkki;
+			this.malli = malli;
+			this.tayttomaara = tayttomaara;
+		}
 		
 		// Toiminnot
 		public void Kaynnisty()
@@ -34,6 +42,14 @@ class Pyykkikone {
 		public void lopetaOhjelma()
 		{
 			System.out.println("Ohjelma on päättynyt");
+		}
+		
+		public void tulostaTiedot()	// Tulostetaan pesukoneen tiedot
+		{
+			System.out.println("Pesukone");
+			System.out.println("Merkki: " + merkki);
+			System.out.println("Malli: " + malli);
+			System.out.println("Täyttömäärä: " + tayttomaara + " kg");
 		}
 	}
 
